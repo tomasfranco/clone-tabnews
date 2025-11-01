6a1d2d2a326eec39c3eeac616d94aaa444127b87
Tomás Franco <tomas.f.carvalho@gmail.com>
2025-11-01 02:51:22 +0000

    adicionei mais 3 linhas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
+    <>
     return <h1>Atualizando informações, estudos para dev.</h1>
+    <h1>Vamos testar mais uma linha</h1>
+    <h2>Preciso ter certeza que ainda lembro como adicionar mais linhas! 😆</h2>
+    </>
+    
 }
 
 export default Home;
