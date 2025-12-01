c758e2171a6380e802b57818a7e3a9874b33727c
Tomás Franco <tomas.f.carvalho@gmail.com>
2025-12-01 00:26:14 +0000

    fix `/migrations` endpoint` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,7 +5,7 @@ import database from "infra/database.js";
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
-    return response.status(405).end({
+    return response.status(405).json({
       error: `Method "${request.method}" not allowed`,
     });
   }
@@ -25,7 +25,6 @@ export default async function migrations(request, response) {
 
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-
       return response.status(200).json(pendingMigrations);
     }
 
