1faa48136fb3ca6f93a2bb132530358b8f4687f4
Tomás Franco <tomas.f.carvalho@gmail.com>
2026-01-06 02:52:11 +0000

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,17 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(errro, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando o Postgres aceitar conexões");
+checkPostgres();
