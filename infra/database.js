b5ca2fad88dd70b924938af89d5234e95701bd5a
Tomás Franco <tomas.f.carvalho@gmail.com>
2026-01-18 01:51:59 +0000

    fix `eslint` linting

--- infra/database.js
+++ infra/database.js
@@ -38,7 +38,9 @@ function getSSLValues() {
   return process.env.NODE_ENV === "production" ? true : false;
 }
 
-export default {
+const database = {
   query,
   getNewClient,
 };
+
+export default database;
