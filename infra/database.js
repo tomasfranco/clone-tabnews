f805e13ebdbbee353ceb9dd8e968d6c8a63ea2d0
Tomás Franco <tomas.f.carvalho@gmail.com>
2026-03-22 09:39:54 -0300

    Fix env postgres user (#35)
    ## 🧠 O que foi feito
    
    - Correção do erro `getHandler is not defined`
    - Refatoração das rotas de migrations (GET/POST)
    - Padronização do tratamento de erros
    - Melhoria na conexão com banco de dados
    - Criação de teste de integração para `/status`
    
    ## 🧪 Testes realizados
    
    - ✅ API funcionando normalmente (`200 OK`)
    - ❌ Simulação de erro de autenticação no Postgres (`28P01`)
    - 🔥 Validação do retorno de erro padronizado (`500 InternalServerError`)
    
    ## 🎯 Objetivo
    
    Validar comportamento da aplicação em cenários reais de falha de infraestrutura (database auth failure).
    
    ## ⚠️ Observação
    
    Alteração de `.env` foi feita intencionalmente para simulação de erro e será revertida após validação.

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServicerError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("/n Erro dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServicerError({
+      message: "Erro na conexão com o Banco ou na Query (Consulta).",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
